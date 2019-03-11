87178e77495ebe673143cabf3f4c32e0ba1aee9f
prometheusXderpgang <prometheusXderpgang@gmail.com>
2019-03-11 13:35:23 +0530

    Fix build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-make O=out ARCH=arm64 alchemy_defconfig
-make -j$(nproc --all) O=out ARCH=arm64 CC=~/kernel/dtc9/bin/clang CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=~/kernel/gcc9/bin/aarch64-linux-gnu-
+make O=out ARCH=arm64 statix_defconfig
+make -j$(nproc --all) O=out ARCH=arm64 CC=~/clang9 CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=~/gcc9
